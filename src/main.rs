26b7e29f69ce6d54abadeee5f96f9cd7f500b1dc
just_a_mango <horace.hoff@gmail.com>
2023-03-24 19:22:37 +0100

    uh idk

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,41 @@
-use std::io::{stdout, Write};
-use crossterm::{cursor::MoveTo, queue, terminal::SetSize};
+use std::{io::{stdout, Write}, thread, time};
+use crossterm::{cursor::MoveTo, queue, style::{Color, SetForegroundColor, ResetColor}};
 
-use crate::{rendering::render, logic::begin_play};
-mod rendering;
-mod bresenham;
-mod text;
-mod logic;
-
-#[derive(PartialEq, Eq, Clone)]
-pub struct Pixel {
-    lit: bool,
-    color: String,
-    x: u8,
-    y: u8
-}
-
-fn all_pixels_off(buffer: &Vec<Pixel>) -> Vec<Pixel> {
-    let mut out_buffer: Vec<Pixel> = buffer.clone();
-    for pixel in &mut out_buffer {
-        pixel.lit = false;
+fn draw_to_terminal(row: u16, column: u16, color:Color) {
+    if row > 50 || column > 150 {
+        panic!("Invalid pixel position -> x: {}; y: {}", row, column)
     }
-    out_buffer
-}
+    queue!(
+        stdout(),
+        MoveTo(column, row),
+        SetForegroundColor(color),
+        crossterm::style::Print("█"),
+        ResetColor
+    ).expect("Failed to draw to terminal");
 
-fn all_pixels_on(buffer: &Vec<Pixel>, color: Option<&str>) -> Vec<Pixel> {
-    let mut out_buffer: Vec<Pixel> = buffer.clone();
-    for pixel in &mut out_buffer {
-        pixel.lit = true;
-        pixel.color = color.unwrap_or("white").to_string();
-    }
-    out_buffer
+    stdout().flush().unwrap()
 }
 
 fn clear_terminal() {
-    // clear the terminal and purge using crossterm
-    crossterm::terminal::Clear(crossterm::terminal::ClearType::Purge);
+    queue!(stdout(), crossterm::terminal::Clear(crossterm::terminal::ClearType::All)).unwrap();
 }
 
-
-
-fn logic() {
-    let mut pixel_buffer: Vec<Pixel> = Vec::new();
-    let width: u16 = 150;
-    let height: u16 = 50;
-    
-    for y in 0..height {
-        for x in 0..width {
-            let pixel = Pixel {
-                lit: true,
-                color: "white".to_string(),
-                x: x as u8,
-                y: y as u8
-            };
-            pixel_buffer.push(pixel);
-        }
-    }
-    // move cursor to top left
-    queue!(stdout(), MoveTo(0, 0), SetSize(150, 50)).unwrap();
-    render(&pixel_buffer);
-    begin_play(&pixel_buffer);
-}
 
 fn main() {
+    queue!(stdout(), crossterm::terminal::SetSize(150, 300)).unwrap();
     clear_terminal();
-    logic();
-    stdout().lock().write_fmt(format_args!("\x1B[3J")).unwrap();
+    // let now = time::Instant::now();
+    // draw_to_terminal(10, 10);
+    // println!("{}", now.elapsed().as_secs_f64());
+    // draw_to_terminal(1, 1, Color::Red);
+    // loop {
+
+    // }
+    let mut i = 0;
+
+    loop {
+        draw_to_terminal(i,i, Color::DarkGreen);
+        i += 1;
+        thread::sleep(time::Duration::from_millis(100));
+    }
 }
